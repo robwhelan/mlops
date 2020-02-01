13a2351e2f773424d6343cc3441970fce6bd4cb0
Rob Whelan <whelan@gmail.com>
2020-02-01 01:02:43 -0600

    modify repo name to start with AmazonSageMaker

--- setup-mlops.sh
+++ setup-mlops.sh
@@ -1,12 +1,13 @@
 export AWS_PROFILE=2ndwatch
-export STACK_NAME=mlops16
-export PROJECT_NAME='robs-project16'
+export STACK_NAME=mlops-format-name
+export PROJECT_NAME='robs-format-name'
 export ML_ZONE_BUCKET='2ndwatch-data-playground'
 
 export MY_STACK=$(aws cloudformation create-stack \
   --stack-name $STACK_NAME \
   --template-body file://main.yaml \
   --capabilities CAPABILITY_IAM \
+  --disable-rollback \
   --parameters \
     ParameterKey=pMLZoneBucket,ParameterValue=$ML_ZONE_BUCKET \
     ParameterKey=pProjectName,ParameterValue=$PROJECT_NAME \
